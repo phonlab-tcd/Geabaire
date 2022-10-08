76b65c1b70e1145f4ef4e5dbc0686b68a7c3d544
Rían Errity <rian@paradaux.io>
2022-10-08 04:02:53 +0200

    Redux base impl

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import store from "./state/store";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
