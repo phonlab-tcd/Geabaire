c0321be2f526d947aa41dac845fc26107ee51be2
Rían Errity <rian@paradaux.io>
2022-10-09 20:10:34 +0200

    Fix issue with style object

--- components/boards/BoardButton.js
+++ components/boards/BoardButton.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function BoardButton({ item }) {
-    let style = StyleSheet.create({
+    let style = {
         flex: 1,
         borderColor: "rgba(12, 12, 12, 0.3)",
         borderWidth: 1,
         padding: 12,
         marginVertical: 8,
         marginHorizontal: 16,
-    });
+    };
 
     return (
         <TouchableOpacity>
