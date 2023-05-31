978dfe07a717b86de45c0ddaf6b1d3ff5259f3a8
Rían Errity <rian@paradaux.io>
2023-05-31 02:08:56 +0200

    Temporarily remove url polyfill

--- state/supabase.js
+++ state/supabase.js
@@ -1,9 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import { setupURLPolyfill } from "react-native-url-polyfill";
 
 import appjson from "../app.json";
-setupURLPolyfill();
 
 export const supabase = createClient(
     appjson.supabase.url,
