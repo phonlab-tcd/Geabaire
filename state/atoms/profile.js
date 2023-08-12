76acadcf4289fa29ae4775661b96c7a75d6532f7
Rían Errity <rian@paradaux.io>
2023-08-12 19:08:20 +0100

    Add new profile hook / table

---
+++ state/atoms/profile.js
@@ -0,0 +1,33 @@
+import { atom, selector } from "recoil";
+
+export const idState = atom({
+    key: "profileIdState",
+    default: undefined,
+});
+
+export const nameState = atom({
+    key: "profileNameState",
+    default: undefined,
+});
+
+export const canContactState = atom({
+    key: "profileContactState",
+    default: undefined,
+});
+
+export const onOpenBoardState = atom({
+    key: "profileOnOpenBoardState",
+    default: undefined,
+});
+
+export const profileState = selector({
+    key: "sentence",
+    get: ({ get }) => {
+        return {
+            id: get(idState),
+            name: get(nameState),
+            can_contact: get(canContactState),
+            on_open_board: get(onOpenBoardState)
+        }
+    }
+})
