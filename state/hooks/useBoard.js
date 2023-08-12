5383df76074b61b8f2e0e7de94fd590cf4d5dcdd
Rían Errity <rian@paradaux.io>
2023-08-12 19:08:32 +0100

    Begin setting up useBoard

--- state/hooks/useBoard.js
+++ state/hooks/useBoard.js
@@ -1,9 +1,26 @@
 import { useRecoilState } from "recoil"
 import { boardStackState, loadedBoardState } from "../atoms/boards"
+import { useEffect } from "react";
 
-export default function useBoard() {
+// Represents a board in memory and provides helper functions
+// for performing actions on a board
+export default function useBoard(board) {
+    // Where the user has navigated to within a given board
+    // An array of board/child board IDs. 
     const [boardStack, setBoardStack] = useRecoilState(boardStackState);
+
+    // The entire board json object from aac_complete_boards
     const [loadedBoard, setLoadedBoard] = useRecoilState(loadedBoardState);
 
-    return {}
+    // Load the board if it isn't already
+    useEffect(() => {
+        async function load() {
+        }
+
+        if (!loadedBoard || !loadedBoard.meta.id === board) {
+            load();
+        }
+    }, [])
+
+    return {x: ""}
 }
