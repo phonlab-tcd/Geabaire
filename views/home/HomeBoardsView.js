543d0a1e54a79ce4a6cc86bbb0c2d0c34dc8dcfa
Rían Errity <rian@paradaux.io>
2023-11-14 14:27:34 +0000

    Clean up

--- views/home/HomeBoardsView.js
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-
-export default function HomeBoardsView() {
-    return (
-        <SafeAreaView>
-            
-        </SafeAreaView>
-    )
-}
